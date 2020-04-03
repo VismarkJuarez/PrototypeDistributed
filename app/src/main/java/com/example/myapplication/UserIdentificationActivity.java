12682da76484c6e2252688b18afaaf6516a18e0d
VismarkJuarez <vismarkjuarez@gmail.com>
2020-04-03 04:52:18 -0500

    Added toggling functionality to the instructor and student buttons.  An  statement has been added on the UserType struct to guard against NPEs.

--- app/src/main/java/com/example/myapplication/UserIdentificationActivity.java
+++ app/src/main/java/com/example/myapplication/UserIdentificationActivity.java
@@ -3,12 +3,95 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.Models.UserType;
 
 public class UserIdentificationActivity extends AppCompatActivity {
+
+    //Listing relevant Activity buttons and input values
+    private UserType userType = null;
+    private String userName = null;
+    private Button instructorButton, studentButton, submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_identification);
+
+        /**
+        * Mapping member variable buttons to the appropriate UI Button components.
+        * The `userName` and `userType` variable values will be set when the
+        * `submitButton` gets clicked; so their default values are left as null
+        * for now.
+        * */
+        instructorButton = (Button)findViewById(R.id.instructorButton);
+        studentButton = (Button)findViewById(R.id.studentButton);
+        submitButton = (Button)findViewById(R.id.submitUserDetailsButton);
+
+        //setup onClick listeners for all buttons
+        setupOnClickListeners();
+    }
+
+    /**
+     * A wrapper function for configuring all of the
+     * button clicks for this activity.
+     */
+    private void setupOnClickListeners() {
+
+        /**
+         * onClick configuration for the `instructorButton`.
+         * When the user clicks on this button, the button's
+         * color will change to turquoise to indicate that it has
+         * been selected.
+         */
+        instructorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /**
+                 * First, check to see if a `userType` has been set to STUDENT.
+                 * If so, remove the turquoise coloring of the `studentButton`.
+                 * */
+                if(UserType.STUDENT.equals(userType)) {
+                    studentButton.setBackgroundResource(android.R.drawable.btn_default);
+                }
+
+                //Now, set the instructorButton color to turquoise
+                instructorButton.setBackgroundColor(getResources().getColor(R.color.turquoise));
+
+                //Finally, set the value of `userType` to UserType.INSTRUCTOR
+                userType = UserType.INSTRUCTOR;
+            }
+        });
+        
+
+        /**
+         * onClick configuration for the `studentButton`.
+         *
+         * When the user clicks on this button, the button's
+         * color will change to turquoise to indicate that it
+         * has been selected.
+         */
+        studentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /**
+                 * First, check to see if a `userType` has been set to INSTRUCTOR.
+                 * If so, remove the turquoise coloring of the `instructorButton`.
+                 * */
+                if(UserType.INSTRUCTOR.equals(userType)) {
+                    instructorButton.setBackgroundResource(android.R.drawable.btn_default);
+                }
+
+                //Now, set the color of the studentButton to turquoise
+                studentButton.setBackgroundColor(getResources().getColor(R.color.turquoise));
+
+                //Finally, set the value of `userType` to UserType.STUDENT
+                userType = UserType.STUDENT;
+            }
+        });
     }
 }
